553c7ea7bb955d2241f7e226aa5ccd8eb49fefb9
Akash <akashuhulekal@gmail.com>
2022-07-18 22:31:10 +0530

    made it into a single compo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import React, { useRef, useState} from "react";
-import './App.css';
+import React from 'react'
+import Home from './Home'
 
-function App() {
-  const filePickerRef = useRef();
-
-  const ImageHandler = () => {
-    filePickerRef.current.click();
-  };
-  const [image, setimage] = useState()
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-  }
-  const changeHandle=(e)=>{
-    if (e.target.files && e.target.files.length===1){
-      const pickedfile = e.target.files[0];
-      setimage(pickedfile);
-      console.log(image)
-      let reader = new FileReader();
-      reader.onloadend = function() {
-        console.log(reader.result)
-    }
-    reader.readAsDataURL(pickedfile);
-  
-    } 
-
-  }
+const App = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" accept='.jpg,.png,.jpeg' onChange={changeHandle}     style={{ display: "none" }}  ref={filePickerRef}/>
-      <button type="button" onClick={ImageHandler}>
-          Pick image
-        </button>
-    </form>
-  );
+    <Home/>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Home.js
@@ -0,0 +1,41 @@
+import './App'
+
+
+
+import React, { useRef, useState} from "react";
+
+function Home() {
+  const filePickerRef = useRef();
+
+  const ImageHandler = () => {
+    filePickerRef.current.click();
+  };
+  const [image, setimage] = useState()
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+  const changeHandle=(e)=>{
+    if (e.target.files && e.target.files.length===1){
+      const pickedfile = e.target.files[0];
+      setimage(pickedfile);
+      console.log(image)
+      let reader = new FileReader();
+      reader.onloadend = function() {
+        console.log(reader.result)
+    }
+    reader.readAsDataURL(pickedfile);
+  
+    } 
+
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" accept='.jpg,.png,.jpeg' onChange={changeHandle}     style={{ display: "none" }}  ref={filePickerRef}/>
+      <button type="button"  onClick={ImageHandler}>
+          Pick image
+        </button>
+    </form>
+  );
+}
+
+export default Home;
